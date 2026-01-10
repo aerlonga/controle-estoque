d7a92ab74f5e4e1597a7164c84802075953992b8
aerlonga <aerlon.as@gmail.com>
2026-01-10 17:29:48 -0300

    salvando

--- frontend/src/dashboard/Dashboard.tsx
+++ frontend/src/dashboard/Dashboard.tsx
@@ -6,10 +6,10 @@ import { alpha } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
-import AppNavbar from './componentsMuUI/AppNavbar';
-import Header from './componentsMuUI/Header';
-import MainGrid from './componentsMuUI/MainGrid';
-import SideMenu from './componentsMuUI/SideMenu';
+import AppNavbar from './components/AppNavbar';
+import Header from './components/Header';
+import MainGrid from './components/MainGrid';
+import SideMenu from './components/SideMenu';
 import AppTheme from '../shared-theme/AppTheme';
 import {
   chartsCustomizations,
